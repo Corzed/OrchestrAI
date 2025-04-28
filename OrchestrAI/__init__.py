415cfe97384c77a3424b84f4f9aa4eb87c267934
Corzed <140441258+Corzed@users.noreply.github.com>
2025-04-28 18:04:12 -0400

    Update __init__.py

--- OrchestrAI/__init__.py
+++ OrchestrAI/__init__.py
@@ -2,6 +2,6 @@ from .agent import Agent
 from .agent_manager import AgentManager
 from .agent_tool import AgentTool
 from .logging_utils import log_message, spinner
-from .models import ToolModel, ActionModel, AIResponseModel, AGENT_RESPONSE_SCHEMA
+from .models import ToolModel, ActionModel, AIResponseModel, AGENT_RESPONSE_JSON_SCHEMA
 
-__version__ = "0.3.5"
+__version__ = "0.5"
